9c0f72ab1dfeb20fa9a71e184d984e948f1194eb
10113hajun <mohazi1250@gmail.com>
2025-11-03 13:17:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import streamlit as st
 st.title('웹서비스제작')
 name=st.text_input('이름을 입력하세요:')
-st.selectbox('좋아하는 음식을 선택하세요:',['김치찌개','된장찌개'])
+menu=st.selectbox('좋아하는 음식을 선택하세요:',['김치찌개','된장찌개'])
 if st.button('인사말 생성'):
   st.info(name+'님 안녕하세요')
   st.warning('반가워요')
-  st.error('천만해요')
+  st.error(menu+'를 좋아하시나봐요 저도 좋아해요')
   st.balloons()
